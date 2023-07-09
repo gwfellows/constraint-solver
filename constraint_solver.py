33a9b25d6b2c2793ad3b74b7a551d02bb6caeab4
Grant Fellows <33993581+gwfellows@users.noreply.github.com>
2023-07-09 12:35:13 -0700

    solve the constraints

--- constraint_solver.py
+++ constraint_solver.py
@@ -12,16 +12,39 @@ def mixed_second_derivative(f, vec, i1, i2, dx = DX):
 			for idx, val in enumerate(vec)]
 	def first_derivative(v1, v2):
 		return ( f(vec_replaced(v1+dx, v2)) - f(vec_replaced(v1, v2))) / dx
-	def second_derivative(v1, v2):
-		return   ( first_derivative(v1, v2+dx) - first_derivative(v1, v2) ) / dx
-	return second_derivative
+	if i1==i2:
+		return  ( first_derivative(vec[i1]+dx, vec[i2]) - first_derivative(vec[i1], vec[i2]) ) / dx
+	return   ( first_derivative(vec[i1], vec[i2]+dx) - first_derivative(vec[i1], vec[i2]) ) / dx
 
+# should be collob vector I think
+def gradient(f,vex):
 '''
 print(
-	mixed_second_derivative(lambda v: sin(v[0])*v[1]**2, [10,10], 0, 1)(143,10.5)
+	mixed_second_derivative(lambda v: sin(v[0])*v[1]**2, [143,10.5], 0, 1)
 	)'''
 
-#  he entry of the ith row and the jth column in the hessian for function f at vector v
-def hessian_item(f, i, j, vec):
-	return ( d2_v(f) ) / ()
-	
+def hessian(f, vec):
+	l = len(vec)
+	H = np.zeros((l,l))
+	for i in range(l):
+		for j in range(l):
+			H[j][i] = mixed_second_derivative(f, vec, i, j)
+	return H
+
+def newtons_method(f, vec_start, n_iters):
+	guess = vec_start
+	for _ in n_iters:
+		guess = guess - np.linalg.inv(hessian(f, guess))v@
+		
+"""
+def f(v):
+	x, y, z = v
+	return 2*y*x**4 + 2*y**3 + 2*x*z**3
+	"""
+
+'''
+print(np.linalg.det(hessian(f, [-1,-1,-1]))) i think it works https://www.allmath.com/hessian-matrix-calculator.php#examples-of-hessian-matrix
+print(hessian(f, [1,1,1]))
+print(hessian(f, [-1,-1,-1]))
+'''
+
