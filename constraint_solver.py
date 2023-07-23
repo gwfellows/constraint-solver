5038de16c55ee135424a9f69ba0fa543e9566cde
Grant Fellows <33993581+gwfellows@users.noreply.github.com>
2023-07-22 20:20:24 -0700

    clean up

--- constraint_solver.py
+++ constraint_solver.py
@@ -24,12 +24,8 @@ def gradient(f,vec, dx =  DX):
 			  ( f(vec_replaced(idx, vec[idx]+dx)) - f(vec) ) / dx
 		)
 	return np.c_[grad]
-'''
-print(
-	mixed_second_derivative(lambda v: sin(v[0])*v[1]**2, [143,10.5], 0, 1)
-	)'''
 
-# works
+# works, should optomize by copying diagnal
 def hessian(f, vec):
 	l = len(vec)
 	H = np.zeros((l,l))
@@ -37,12 +33,6 @@ def hessian(f, vec):
 		for j in range(l):
 			H[j][i] = mixed_second_derivative(f, vec, i, j)
 	return H
-
-# just for testing, seems to work
-'''
-def f(v):
-	x, y = v
-	return (x-5)**2+(y-124.3)**2+10'''
 
 #damped newtons method
 def newtons_method(f, vec_start, n_iters, damping_factor = 0.01, learning_rate = 0.001):
@@ -67,41 +57,17 @@ def angle_constraint(x1, y1, x2, y2, x3, y3, angle):
 
 def f(v):
 	x1, y1, x2, y2 = v
-	return equality_constraint(x1, 150) + equality_constraint(y1, 150) + distance_constraint(x1, y1, x2, y2, 100)
+	return equality_constraint(x1, 250) + equality_constraint(y1, 250) + distance_constraint(x1, y1, x2, y2, 100)
 	
-#print (['{0:.10f}'.format(a) for a in newtons_method(f, [10,234, 32, 23], 10000)])
-
 import pyglet
 from pyglet.window import mouse
 
 window = pyglet.window.Window()
 
-circle = pyglet.shapes.Circle(x=100, y=150, radius=10, color=(50, 225, 30))
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
-
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-	print(x,y)
-	if button == mouse.LEFT:
-		print('The left mouse button was pressed.')
-
 x1, y1, x2, y2 = 0,0,0,0
 hilighted1 = False
 hilighted2 = False
 itr = newtons_method(f, [100,234, 320, 23], 10000, learning_rate = 0.01)
-
-
-
-'''
-@window.event
-def on_mouse_motion(x, y, dx, dy):
-    circle = pyglet.shapes.Circle(x=x1, y=y1, radius=20, color=(20, 225, 30))
-    circle.draw()
-    pass'''
 
 def on_mouse_press(x, y, button, modifiers):
     pass
@@ -112,9 +78,7 @@ def on_mouse_release(x, y, button, modifiers):
 @window.event
 def on_mouse_motion(x, y, dx, dy):
 	global hilighted1, hilighted2
-	#print("mouse moved")
 	if (x1+10 > x > x-10) and  (y1+10 > y > y1-10):
-		print("x1 hilighted")
 		hilighted1 = True
 	else:
 		hilighted1 = False
@@ -126,7 +90,6 @@ def on_mouse_motion(x, y, dx, dy):
 @window.event
 def on_mouse_drag(x, y, dx, dy, *args):
 	global x1, y1, x2, y2, itr
-	#on_mouse_motion(x,y,dx,dy)
 	if hilighted1:
 		x1 = x
 		y1 = y
@@ -135,66 +98,18 @@ def on_mouse_drag(x, y, dx, dy, *args):
 		y2 = y
 	itr = newtons_method(f, [x1,y1, x2, y2], 10000, learning_rate = 0.01)
 
-@window.event
-def on_draw():
-    #window.clear()
-    #label.draw()
-    #circle.draw()
-	pass
-
-
-#pyglet.app.run()
 def update(dt):
 	global x1, y1, x2, y2
 	window.clear()
-	#print("run")
 	x1, y1, x2, y2 = next(itr)
-	print(hilighted1)
 	if hilighted1:
-		#print("--------------------")
 		pyglet.shapes.Circle(x=x1, y=y1, radius=15, color=(20, 225, 30)).draw()
 		pyglet.shapes.Circle(x=x1, y=y1, radius=13, color=(0, 0, 0)).draw()
 	if hilighted2:
-		#print("--------------------")
 		pyglet.shapes.Circle(x=x2, y=y2, radius=15, color=(20, 225, 30)).draw()
 		pyglet.shapes.Circle(x=x2, y=y2, radius=13, color=(0, 0, 0)).draw()
-	circle1 = pyglet.shapes.Circle(x=x1, y=y1, radius=10, color=(50, 225, 30))
-	circle2 = pyglet.shapes.Circle(x=x2, y=y2, radius=10, color=(50, 225, 30))
-	#circle.draw()
-	circle1.draw()
-	circle2.draw()
-	#window.clear()
-'''
-def hilight(a):
-	window.clear()
-	print("--------------------4----------")
-	print(x1, y1)
-	pyglet.shapes.Circle(x=x2, y=y2, radius=10, color=(20, 225, 30)).draw()
-	pyglet.shapes.Circle(x=x1, y=y1, radius=15, color=(20, 225, 30)).draw()
-	pyglet.shapes.Circle(x=x1, y=y1, radius=13, color=(0, 0, 0)).draw()
-	pyglet.shapes.Circle(x=x1, y=y1, radius=10, color=(20, 225, 30)).draw()'''
+	pyglet.shapes.Circle(x=x1, y=y1, radius=10, color=(50, 225, 30)).draw()
+	pyglet.shapes.Circle(x=x2, y=y2, radius=10, color=(50, 225, 30)).draw()
 
-import time
-#pyglet.clock.schedule_interval(hilight, 1/60.0)
 pyglet.clock.schedule_interval(update, 1/60.0)
-
 pyglet.app.run()
-
-
-
-'''
-def f(v):
-	x, y, z = v
-	return 2*y*x**4 + 2*y**3 + 2*x*z**3'''
-	
-# print(gradient(f, [1,1,1])) works
-
-
-'''
-print(np.linalg.det(hessian(f, [-1,-1,-1]))) i think it works https://www.allmath.com/hessian-matrix-calculator.php#examples-of-hessian-matrix
-print(hessian(f, [1,1,1]))
-print(hessian(f, [-1,-1,-1]))
-'''
-
-# now to do gui
-
